767c225476214c82251762c329bb4c9d242d2859
Juhwan <dev.juhwanlee@gmail.com>
2022-04-15 18:32:11 +0900

    Initial settings

---
+++ src/components/Post.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Grid, Image, Text } from '../elements';
+
+const Post = (props) => {
+  return (
+    <>
+      <Grid>
+        <Grid is_flex>
+          <Image shape='circle' src={props.src} />
+          <Text bold>{props.user_info.user_name}</Text>
+          <Text>{props.insert_dt}</Text>
+        </Grid>
+        <Grid padding='16px'>
+          <Text>{props.contents}</Text>
+        </Grid>
+        <Grid>
+          <Image shape='rectangle' src={props.src} />
+        </Grid>
+        <Grid padding='16px'>
+          <Text bold>댓글 {props.comment_cnt}개</Text>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+Post.defaultProps = {
+  user_info: {
+    user_name: 'juhwan',
+    user_profile:
+      'https://mimg.segye.com/content/image/2017/07/22/20170722505000.jpg',
+  },
+  image_url:
+    'https://mimg.segye.com/content/image/2017/07/22/20170722505000.jpg',
+  contents: '친칠라네요!',
+  comment_cnt: 10,
+  insert_dt: '2022-04-14 13:17:00',
+};
+
+export default Post;
