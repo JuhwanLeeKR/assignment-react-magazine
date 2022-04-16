fbfa70588a25e8b725707e2f17fadf2a8698e7ba
Juhwan <dev.juhwanlee@gmail.com>
2022-04-16 17:58:32 +0900

    Feat: upload img

---
+++ src/redux/modules/image.js
@@ -0,0 +1,54 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+import { storage } from '../../shared/firebase';
+
+const UPLOADING = 'UPLOADING';
+const UPLOAD_IMAGE = 'UPLOAD_IMAGE';
+
+const uploading = createAction(UPLOADING, (uploading) => ({ uploading }));
+const uploadImage = createAction(UPLOAD_IMAGE, (image_url) => ({ image_url }));
+
+const initialState = {
+  image_url: '',
+  uploading: false,
+};
+
+export default handleActions(
+  {
+    [UPLOAD_IMAGE]: (state, action) =>
+      produce(state, (draft) => {
+        draft.image_url = action.payload.image_url;
+        draft.uploading = false;
+      }),
+    [UPLOADING]: (state, action) =>
+      produce(state, (draft) => {
+        draft.uploading = action.payload.uploading;
+      }),
+  },
+  initialState
+);
+
+const uploadImageFB = (image) => {
+  return function (dispatch, getState, { history }) {
+    dispatch(uploading(true));
+
+    const _upload = storage.ref(`images/${image.name}`).put(image);
+
+    _upload.then((snapshot) => {
+      console.log(snapshot);
+
+      snapshot.ref.getDownloadURL().then((url) => {
+        dispatch(uploading(url));
+        console.log(url);
+      });
+    });
+  };
+};
+
+const actionCreators = {
+  uploadImage,
+  uploadImageFB,
+};
+
+export { actionCreators };
