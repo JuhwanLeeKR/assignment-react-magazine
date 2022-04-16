3f53dcb532beb47456469b3e1f44d4c8edbd350e
Juhwan <dev.juhwanlee@gmail.com>
2022-04-16 13:45:26 +0900

    Add files: components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Button, Grid, Input, Text } from '../elements';
-import { setCookie } from '../shared/Cookie';
+import { Text, Input, Grid, Button } from '../elements';
+import { getCookie, setCookie, deleteCookie } from '../shared/Cookie';
 
 import { useDispatch } from 'react-redux';
 import { actionCreators as userActions } from '../redux/modules/user';
+import { emailCheck } from '../shared/common';
 
 const Login = (props) => {
   const dispatch = useDispatch();
@@ -12,47 +13,56 @@ const Login = (props) => {
   const [pwd, setPwd] = React.useState('');
 
   const login = () => {
+    console.log(id);
+
     if (id === '' || pwd === '') {
       window.alert('아이디 혹은 비밀번호가 공란입니다! 입력해주세요!');
       return;
     }
+
+    if (!emailCheck(id)) {
+      window.alert('이메일 형식이 맞지 않습니다!');
+      return;
+    }
+
     dispatch(userActions.loginFB(id, pwd));
   };
 
   return (
     <React.Fragment>
       <Grid padding='16px'>
-        <Text type='heading'>로그인 페이지</Text>
-      </Grid>
-      <Grid padding='16px'>
-        <Input
-          // value={id}
-          label='아이디'
-          _onChange={(e) => {
-            setId(e.target.value);
-          }}
-          placeholder='아이디를 입력하세요.'
-        />
-        <Input
-          // value={pwd}
-          label='패스워드'
-          _onChange={(e) => {
-            setPwd(e.target.value);
-          }}
-          type='password'
-          placeholder='비밀번호를 입력하세요.'
-        />
-      </Grid>
-      <Grid padding='16px'>
+        <Text size='32px' bold>
+          로그인
+        </Text>
+
+        <Grid padding='16px 0px'>
+          <Input
+            label='아이디'
+            placeholder='아이디를 입력해주세요.'
+            _onChange={(e) => {
+              setId(e.target.value);
+            }}
+          />
+        </Grid>
+
+        <Grid padding='16px 0px'>
+          <Input
+            label='패스워드'
+            placeholder='패스워드 입력해주세요.'
+            type='password'
+            _onChange={(e) => {
+              setPwd(e.target.value);
+            }}
+          />
+        </Grid>
+
         <Button
           text='로그인하기'
           _onClick={() => {
             console.log('로그인 했어!');
             login();
           }}
-        >
-          로그인
-        </Button>
+        ></Button>
       </Grid>
     </React.Fragment>
   );
