efb11a0b1a08c8e8f8519ff8b02527ea629587b0
Juhwan <dev.juhwanlee@gmail.com>
2022-04-18 07:21:08 +0900

    Add function: get comments

--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -24,13 +24,40 @@ const initialState = {
   is_loading: false,
 };
 
-const getCommentFB = (post_id) => {
-  return function (dispatch, getState, { history }) {};
+const getCommentFB = (post_id = null) => {
+  return function (dispatch, getState, { history }) {
+    // post_id가 없는 경우 예외처리
+    if (!post_id) {
+      return;
+    }
+    const commentDB = firestore.collection('comment');
+
+    commentDB
+      .where('post_id', '==', post_id)
+      .orderBy('insert_dt', 'desc')
+      .get()
+      .then((docs) => {
+        let list = [];
+
+        docs.forEach((doc) => {
+          list.push({ ...doc.data(), id: doc.id });
+        });
+
+        dispatch(setComment(post_id, list));
+      })
+      .catch((err) => {
+        console.log('댓글 정보를 가져올 수가 없네요!', err);
+      });
+  };
 };
 
 export default handleActions(
   {
-    [SET_COMMENT]: (state, action) => produce(state, (draft) => {}),
+    [SET_COMMENT]: (state, action) =>
+      produce(state, (draft) => {
+        // let data = {[post_id]: com_list, ...}
+        draft.list[action.payload.post_id] = action.payload.comment_list;
+      }),
     [ADD_COMMENT]: (state, action) => produce(state, (draft) => {}),
     [LOADING]: (state, action) =>
       produce(state, (draft) => {
