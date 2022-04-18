b1f6338e25b0309ce57903c5ce01b05a17f99709
Juhwan <dev.juhwanlee@gmail.com>
2022-04-18 13:31:14 +0900

    Add function: notification

--- src/components/NotiBadge.js
+++ src/components/NotiBadge.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { Badge } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
+import { realtime } from '../shared/firebase';
+import { useSelector } from 'react-redux';
+
 const NotiBadge = (props) => {
   const [is_read, setIsRead] = React.useState(true);
+  const user_id = useSelector((state) => state.user.user.uid);
   const notiCheck = () => {
+    const notiDB = realtime.ref(`noti/${user_id}`);
+    notiDB.update({ read: true });
     props._onClick();
   };
+
+  React.useEffect(() => {
+    const notiDB = realtime.ref(`noti/${user_id}`);
+
+    notiDB.on('value', (snapshot) => {
+      setIsRead(snapshot.val().read);
+    });
+
+    return () => notiDB.off();
+  }, []);
+
   return (
     <>
       <Badge
